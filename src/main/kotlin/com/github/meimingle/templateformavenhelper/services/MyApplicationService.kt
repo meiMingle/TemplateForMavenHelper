9d6d8f56da2bc3de83198158a2752b80a0a38dd6
GitHub Action <action@github.com>
2022-09-08 14:54:22 +0000

    Template cleanup

--- src/main/kotlin/com/github/meimingle/templateformavenhelper/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/meimingle/templateformavenhelper/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.meimingle.templateformavenhelper.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.meimingle.templateformavenhelper.MyBundle
 
 class MyApplicationService {
 
